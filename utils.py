91132bf52d9b4664369a482d7f3b2f0c5951966b
Nguyen Quoc Dung <51025577+dungnq271@users.noreply.github.com>
2022-08-29 14:03:46 +0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import math as m
-
+import pygame
 
 def findDistance(x1, y1, x2, y2):
     dist = m.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -14,7 +14,12 @@ def findAngle(x1, y1, x2, y2):
     return degree
 
 
-def sendWarning(x):
+audio = 'sound_warning\mixkit-alarm-clock-beep-988.wav'
+def sendWarning():
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio)
+    pygame.mixer.music.play()
+
     pass
 
 
